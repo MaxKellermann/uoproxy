eeb519b82b15b39c5458102d8e8f9c13fc8d5587
Max Kellermann <max@duempel.org>
2005-12-08 22:12:57 +0000

    delete items and mobiles memory on mapchange
    git-svn-id: svn://svn.berlios.de/uoproxy/trunk@149 546381f1-97f9-0310-94fd-acf1737c2c57

--- src/shandler.c
+++ src/shandler.c
@@ -426,6 +426,10 @@ static packet_action_t handle_extended(struct connection *c,
     case 0x0008:
         if (length <= sizeof(c->packet_map_change))
             memcpy(&c->packet_map_change, data, length);
+
+        connection_delete_items(c);
+        connection_delete_mobiles(c);
+
         break;
 
     case 0x0018:
