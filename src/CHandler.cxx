MaxKellermann/uoproxy

commit 9e6503c2b24cc99eff9d9efdfc14fecfd53ff370

CServer: convert more functions to methods

src/CHandler.cxx
@@ -480,12 +480,12 @@ handle_game_login(LinkedServer *ls,
                     reuse_conn->client_version.protocol = c->client_version.protocol;
 
                 /* remove the object from the old connection */
-                connection_server_remove(c, ls);
+                c->Remove(*ls);
                 connection_delete(c);
 
                 LogFormat(2, "attaching redirected client to its previous connection\n");
 
-                connection_server_add(reuse_conn, ls);
+                reuse_conn->Add(*ls);
             } else {
                 /* houston, we have a problem -- reject the game login -- it
                    either came in too slowly (and so we already reaped the
@@ -572,16 +572,16 @@ handle_play_server(LinkedServer *ls,
     c2 = find_attach_connection(c);
     if (c2 != nullptr) {
         /* remove the object from the old connection */
-        connection_server_remove(c, ls);
+        c->Remove(*ls);
         connection_delete(c);
 
         if (c2->instance->config->razor_workaround) { ///< need to send redirect
             /* attach it to the new connection and send redirect (below) */
             ls->attaching = true;
-            connection_server_add(c2, ls);
+            c2->Add(*ls);
         }  else {
             /* attach it to the new connection and begin playing right away */
-            attach_after_play_server(c2, ls);
+            c2->Add(*ls);
         }
 
         retaction = PacketAction::DROP;

src/Attach.cxx
@@ -211,7 +211,7 @@ attach_after_play_server(Connection *c, LinkedServer *ls)
 
     LogFormat(2, "attaching connection\n");
 
-    connection_server_add(c, ls);
+    c->Add(*ls);
 
     attach_send_world(ls);
 }

src/CServer.cxx
@@ -27,39 +27,28 @@
 #include <stdlib.h>
 
 void
-connection_server_add(Connection *c, LinkedServer *ls)
+Connection::Add(LinkedServer &ls) noexcept
 {
-    assert(ls->connection == nullptr);
+    assert(ls.connection == nullptr);
 
-    c->servers.push_front(*ls);
-    ls->connection = c;
+    servers.push_front(ls);
+    ls.connection = this;
 }
 
 void
-connection_server_remove(Connection *c, LinkedServer *ls)
+Connection::Remove(LinkedServer &ls) noexcept
 {
-    connection_check(c);
-    assert(ls != nullptr);
-    assert(c == ls->connection);
+    assert(ls.connection == this);
 
-    connection_walk_server_removed(&c->walk, ls);
+    connection_walk_server_removed(&walk, &ls);
 
-    ls->connection = nullptr;
-    ls->unlink();
-}
-
-LinkedServer *
-connection_server_new(Connection *c, int fd)
-{
-    auto *ls = new LinkedServer(fd);
-    connection_server_add(c, ls);
-    return ls;
+    ls.connection = nullptr;
+    ls.unlink();
 }
 
 void
 connection_server_dispose(Connection *c, LinkedServer *ls)
 {
-    connection_server_remove(c, ls);
-
+    c->Remove(*ls);
     delete ls;
 }

src/Connection.cxx
@@ -46,12 +46,8 @@ int connection_new(Instance *instance,
                       protocol_name(c->client_version.protocol));
     }
 
-    auto *ls = connection_server_new(c, server_socket);
-    if (ls == nullptr) {
-        ret = errno;
-        delete c;
-        return ret;
-    }
+    auto *ls = new LinkedServer(server_socket);
+    c->Add(*ls);
 
     connection_check(c);
 

src/Connection.hxx
@@ -104,6 +104,9 @@ struct Connection final : IntrusiveListHook, UO::ClientHandler {
     Connection(const Connection &) = delete;
     Connection &operator=(const Connection &) = delete;
 
+    void Add(LinkedServer &ls) noexcept;
+    void Remove(LinkedServer &ls) noexcept;
+
     /* virtual methods from UO::ClientHandler */
     bool OnClientPacket(const void *data, size_t length) override;
     void OnClientDisconnect() noexcept override;
@@ -144,15 +147,6 @@ connection_broadcast_divert(Connection *c,
 
 
 /* server list */
-
-void
-connection_server_add(Connection *c, LinkedServer *ls);
-
-void
-connection_server_remove(Connection *c, LinkedServer *ls);
-
-LinkedServer *
-connection_server_new(Connection *c, int fd);
 
 void
 connection_server_dispose(Connection *c, LinkedServer *ls);

commit d91d7853dadbe95630ba795ed3ff7e174e920ad0

CHandler: restore attach_after_play_server() call
Regression by commit 9e6503c2b24cc99eff9d9efdfc14fecfd53ff370

src/CHandler.cxx
@@ -580,7 +580,7 @@ handle_play_server(LinkedServer *ls,
             c2->Add(*ls);
         }  else {
             /* attach it to the new connection and begin playing right away */
-            c2->Add(*ls);
+            attach_after_play_server(c2, ls);
         }
 
         retaction = PacketAction::DROP;